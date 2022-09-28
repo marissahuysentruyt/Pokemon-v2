b51e5ddd5948898dc3fc80b0be46eb00b7e6a468
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2022-09-28 07:34:15 -0400

    :construction: WIP: begin React implementation
    -Create new public folder for React
    -Add an App.js file with ideas (commented out)
    -Create progress-bar.js & css files with ideas (commented out)
    
    pkm-2

---
+++ src/js/progress-bar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProgressBard extends React.Component {
+  render () {
+    return (
+      <div className='header__progress-bar'>
+        <div id='hp-bar'>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Progress-Bar;
